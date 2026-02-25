b204a6240037b92a0d7d7b894bc98804d0a5ffa1
katjurd <kkatherine.jurado@gmail.com>
2026-02-25 09:19:38 -0500

    retry

--- login.php
+++ login.php
@@ -107,7 +107,7 @@
 
   <!-- Left: Image Section (Hidden on small screens) -->
   <div class="hidden md:block md:w-1/2 bg-center rounded-r-[50px] bg-[#1F1F21]">
-      <img src="images/healthyGenerations.png"
+      <img src="images/login-side-image.jpg"
             alt="Healthy Generations"
             style="height: 100%;">
   </div>

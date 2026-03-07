8b4e55607a8b9656776f61e99a832ff38e5f9369
katjurd <kkatherine.jurado@gmail.com>
2026-03-07 14:46:59 -0500

    sign up link changes

--- login.php
+++ login.php
@@ -167,7 +167,7 @@
       <!-- Sign Up Section -->
       <p class="text-center text-gray-700">
         Don’t have an account?
-        <a href="VolunteerRegister.php" class="text-[#5283c7] font-semibold hover:underline">Sign Up Now</a>
+        <a href="VolunteerRegister.php" target="_blank" class="text-[#5283c7] font-semibold hover:underline">Sign Up Now</a>
       </p>
 
     </div>

2fad22512e3da548d608bc826795fb3aeb04d7d7
katjurd <kkatherine.jurado@gmail.com>
2026-03-13 11:51:52 -0400

    header.php

--- header.php
+++ header.php
@@ -823,9 +823,6 @@ if (date("H:i:s") > "18:19:59") {
 		echo('<div class="navbar">
         <!-- Left Section: Logo & Nav Links -->
         <div class="left-section">
-            <div class="logo-container">
-                <a href="index.php"><img src="images/healthyGenPageLogo.png"></a>
-            </div>
                 <!--<a href="viewCheckInOut.php" style="color: white; text-decoration: none;"><div class="date-box">Check In/Out</div></a>-->
             <div class="nav-links">
                 <div class="nav-item">Rides

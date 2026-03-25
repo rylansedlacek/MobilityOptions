c56424cd5b51bde9862e570568c169e81552958a
katjurd <kkatherine.jurado@gmail.com>
2026-03-25 03:00:55 -0400

    complete trip changes

--- completeTrips.php
+++ completeTrips.php
@@ -42,14 +42,13 @@ include 'database/dbEvents.php';
         $events = get_all_events();
         if (sizeof(get_all_events())): ?>
             <div class="table-wrapper">
-                <label>Finalize the ride by selecting "Schedule".</label>
+                <label>Finalize the ride by selecting "Complete".</label> <br/>
                 <table class="general">
                     <thead>
                         <tr>
                             <th>Rider Name</th>
                             <th>Date Of Ride</th>
                             <th>Pick Up Time</th>
-                            <th>Assigned Driver</th>
                             <th>Complete Trip</th>
                             <th style="width:1px"></th>
                         </tr>

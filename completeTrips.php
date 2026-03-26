0c65f17210fe028561240ea66bc3bbeaa98c34ae
katjurd <kkatherine.jurado@gmail.com>
2026-03-26 17:07:40 -0400

    return button changes

--- completeTrips.php
+++ completeTrips.php
@@ -86,7 +86,7 @@ include 'database/dbEvents.php';
         <?php else: ?>
             <p class="no-events standout">There are currently no requests available to view.<a class="button add" href="addEvent.php">Create a New Event</a> </p>
         <?php endif ?>
-        <a class="button cancel" href="eventManagement.php">Return to Dashboard</a>
+        <a class="button return" href="eventManagement.php">Return to Dashboard</a>
     </main>
 </body>
 

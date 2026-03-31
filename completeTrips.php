eeca6a7109b172f90be4f6b3665e0941f1470709
katjurd <kkatherine.jurado@gmail.com>
2026-03-31 10:23:45 -0400

    get_all_events() instead of get_all_prog_events()

--- completeTrips.php
+++ completeTrips.php
@@ -40,11 +40,11 @@ include 'database/dbPersons.php';
     <h1>Complete Trip</h1>
     <main class="general">
         <?php
-        $events = get_all_prog_events();
+        $events = get_all_events();
         $drivers = get_drivers_with_email();
         $vehicles = get_vehicles();
 
-        if (sizeof(get_all_prog_events()) && sizeof($drivers)): ?>
+        if (sizeof(get_all_events()) && sizeof($drivers)): ?>
             <div class="table-wrapper">
                 <label>Finalize dispatched trips for reporting by selecting "Complete".</label>
                 <table class="general">

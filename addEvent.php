fa85f1f25ee64fc9fddda68bffb0258947eea01e
rylansedlacek <sedlacekrylan@gmail.com>
2026-04-01 13:14:50 -0400

    Added error handling and redirects for dashboards

--- addEvent.php
+++ addEvent.php
@@ -696,7 +696,7 @@
                 </script>
                 <br/>
                 <br/>
-                <center><a class="button cancel" href="index.php">Return to Dashboard</a></center>
+                <center><a class="button cancel" href="eventManagement.php">Return to Ride Management</a></center>
 
                 <?php if (isset($_GET['error']) && $_GET['error'] === 'duplicate'): ?>
                 <script>

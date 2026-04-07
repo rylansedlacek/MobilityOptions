1fdf6e25e5f1e1ddadeb41362714c8c9104c8c12
katjurd <kkatherine.jurado@gmail.com>
2026-04-07 16:09:57 -0400

    header.php changes

--- header.php
+++ header.php
@@ -939,7 +939,6 @@ if (date("H:i:s") > "18:19:59") {
             echo ('<div class="navbar">
         <!-- Left Section: Logo & Nav Links -->
         <div class="left-section">
-                <!--<a href="viewCheckInOut.php" style="color: white; text-decoration: none;"><div class="date-box">Check In/Out</div></a>-->
             <div class="nav-links">
                 <div class="nav-item">
                     <p style="color: #4f7dbe;">Quick Access</p>
@@ -1016,64 +1015,67 @@ if (date("H:i:s") > "18:19:59") {
             echo ('<div class="navbar">
         <!-- Left Section: Logo & Nav Links -->
         <div class="left-section">
-            <div class="logo-container">
-                <a href="index.php"><img src="images/healthyGenPageLogo.png" alt="Logo"></a>
-            </div>
             <div class="nav-links">
-                <div class="nav-item">Events
+                <div class="nav-item">
+                    <p style="color: #4f7dbe;">Quick Access</p>
                     <div class="dropdown">
-<a href="viewMyUpcomingEvents.php" style="text-decoration: none;">
+
+<a href="addEvent.php" style="text-decoration: none;">
   <div class="in-nav">
-    <img src="images/list-solid.svg">
-    <span>My Upcoming</span>
+    <img src="images/plus-solid.svg">
+    <span>Schedule a Ride</span>
   </div>
 </a>
 <a href="calendar.php" style="text-decoration: none;">
   <div class="in-nav">
-    <img src="images/new-event.svg">
-    <span>Sign-Up</span>
+    <img src="images/list-solid.svg">
+    <span>View Scheduled Rides</span>
   </div>
 </a>
 <a href="editHours.php" style="text-decoration: none;">
   <div class="in-nav">
     <img src="images/clock-regular.svg">
-    <span>Edit Hours</span>
+    <span>Change Scheduled Ride Time</span>
   </div>
 </a>
-                   </div>
-                </div>
-                <div class="nav-item">
-                    <div class="dropdown">
-<a href="volunteerViewGroup.php" style="text-decoration: none;">
+<a href="viewPendingApps.php" style="text-decoration: none;">
   <div class="in-nav">
-    <img src="images/group.svg">
-    <span>My Groups</span>
+    <img src="images/users-solid.svg">
+    <span>Pending Applications</span>
+  </div>
+</a>
+<a href="adminViewingEvents.php" style="text-decoration: none;">
+  <div class="in-nav">
+    <img src="images/list-solid.svg">
+    <span>Edit Ride Details</span>
   </div>
 </a>
                     </div>
-               </div>
+                </div>
             </div>
         </div>
-
+        <!-- Center Section: Logo -->
+        <div class="center-section">>
+            <a href="index.php" class="logo-container">
+                <img src="images/healthyGenPageLogo.png" alt="Logo">
+            </a>
+        </div>
         <!-- Right Section: Date & Icon -->
         <div class="right-section">
-<a href="calendar.php">
+<!--<a href="calendar.php">
 <div class="icon-butt">
         <svg width="30" height="30" viewBox="0 0 24 24" fill="#C9AB81" xmlns="http://www.w3.org/2000/svg">
             <path d="M3 4C3 3.44772 3.44772 3 4 3H6V2C6 1.44772 6.44772 1 7 1C7.55228 1 8 1.44772 8 2V3H16V2C16 1.44772 16.4477 1 17 1C17.5523 1 18 1.44772 18 2V3H20C20.5523 3 21 3.44772 21 4V21C21 21.5523 20.5523 22 20 22H4C3.44772 22 3 21.5523 3 21V4ZM5 5V20H19V5H5ZM7 10H9V12H7V10ZM11 10H13V12H11V10ZM15 10H17V12H15V10ZM7 14H9V16H7V14ZM11 14H13V16H11V14ZM15 14H17V16H15V14Z"/>
         </svg>
 </div>
-</a>
-            <div class="date-box"></div>
+</a>-->
             <div class="nav-links">
-                <div class="nav-item" style="outline:none;">
+                <div class="nav-item">
                     <div class="icon">
-                        <img src="images/usaicon.png" alt="User Icon">
+                        <img src="images/usaicon.png" alt="User Icon" class="icon-img in-nav-img">
                         <div class="dropdown">
-                            <a href="viewProfile.php" style="text-decoration: none;"><div>View Profile</div></a>
-                            <a href="editProfile.php" style="text-decoration: none;"><div>Edit Profile</div></a>
-                            <a href="changePassword.php" style="text-decoration: none;"><div>Change Password</div></a>
-                            <a href="logout.php" style="text-decoration: none;"><div>Log Out</div></a>
+                            <a href="changePassword.php" class="dropdown-link"><div>Change Password</div></a>
+                            <a href="logout.php" class="dropdown-link"><div>Log Out</div></a>
                         </div>
                     </div>
                 </div>

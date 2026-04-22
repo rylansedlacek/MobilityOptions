4450d46dcbcc9b66bb3de24355b6165ca4b1d640
katjurd <kkatherine.jurado@gmail.com>
2026-04-22 09:42:24 -0400

    nav-item.active color change

--- header.php
+++ header.php
@@ -285,8 +285,8 @@ if (date("H:i:s") > "18:19:59") {
 
         .nav-item:hover,
         .nav-item.active {
-            color: #f5ce7aff;
-            outline: 1px solid #f5d07aff;
+            color: #b9d533;
+            outline: 1px solid #b9d533;
             outline-offset: 7px;
         }
 
